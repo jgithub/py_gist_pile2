efed8d794f623b86dba6d469477f52ae20159d01
jgithub <jgithub1@jeano.net>
2023-11-10 14:31:27 -0600

    Towards boolean_util

--- test/boolean/test_boolean_util.py
+++ test/boolean/test_boolean_util.py
@@ -1,8 +1,18 @@
 import unittest;
+from boolean_util import is_truelike;
 
 class TestBooleanUtil(unittest.TestCase):
   def test_is_truelike(self):
-    self.assertEqual(1, 1)
+    self.assertTrue(is_truelike(True))
+    self.assertTrue(is_truelike("True"))
+    self.assertTrue(is_truelike("t"))
+    self.assertEqual(is_truelike("t"), True)
+    self.assertNotEqual(is_truelike("f"), True)
+    self.assertNotEqual(is_truelike(False), True)
+    self.assertNotEqual(is_truelike("False"), True)
+    self.assertNotEqual(is_truelike("0"), True)
+    self.assertNotEqual(is_truelike(0), True)
+    self.assertEqual(is_truelike(1), True)
 
 if __name__ == '__main__':
     unittest.main()
